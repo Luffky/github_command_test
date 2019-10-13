245e6908c8d708de477d678d8b12644f0f75c695
fukaiyu <fukaiyu@bytedance.com>
2019-10-13 22:21:21 +0800

    third

--- src/main/java/net/kaiyu/MyTestBean.java
+++ src/main/java/net/kaiyu/MyTestBean.java
@@ -14,6 +14,7 @@ public class MyTestBean {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 
 }
